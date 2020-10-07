e3a5c1cddf94c8abac69ae3abb1b123870d745d6
Marco Antonio Almeida Silva <silva_almeida@terceiros.sicredi.com.br>
2020-10-07 18:06:50 -0300

    demonstrando o uso do GIT para Melina

--- sphereAreaAndVolume.cpp
+++ sphereAreaAndVolume.cpp
@@ -6,13 +6,13 @@ int main()
  float raio, area, volume;
  float pi=3.1415;
  
- printf("Digite o raio desejado (cm): ");
+ printf("Digite o raio desejado (m): ");
  scanf("%f", &raio);
  
  area = pi*raio*raio;
  volume = (4*pi*raio*raio*raio)/3.0;
  
- printf("\nA area e o volume sao, respectivamente: %f (cm2) e %f (cm3).\n\n", area, volume);
+ printf("\nA area e o volume sao, respectivamente: %f (m2) e %f (m3).\n\n", area, volume);
  system("PAUSE");
  return 0;   
 }
