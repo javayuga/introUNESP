fd8f5b2dca69d20691bdc1b7cf0e62dafb55fbaa
Melina Almeida <melina.almeida@unesp.br>
2020-10-30 19:31:57 -0300

    Exercicio 5 e 6

---
+++ EX5L6.c
@@ -0,0 +1,29 @@
+#include<stdlib.h>
+#include<stdio.h>       //numeros inteiros positivos e ñ nulos
+int main()
+{
+ int valor, i;
+ 
+ printf("Insira um valor inteiro, positivo e nao nulo ");
+ scanf("%d", &valor);
+ 
+ if(valor%2==0){
+  valor--;
+ }
+ 
+ for(i=valor;i>=3;i=i-2){
+   if(valor%i==0){
+     
+                                           
+ }
+ printf("2,1\n");
+
+ 
+ 
+ 
+  
+ 
+ system("PAUSE");
+ return 0;   
+    
+}
