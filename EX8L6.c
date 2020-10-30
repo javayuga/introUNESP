4d6b4ee58c944dd72de699bb6cb6338ada512514
Marco Antonio Almeida Silva <silva_almeida@terceiros.sicredi.com.br>
2020-10-30 17:22:27 -0300

    escrevi o pseudo-código do ex 8 L 6

--- EX8L6.c
+++ EX8L6.c
@@ -4,6 +4,28 @@
 int main()
 {
     int dado_1, dado_2, placar_1, placar_2;
+	
+	// placar_1 zerado
+	// placar_2 zerado
+	
+	// enquanto (while) placar_1 não for 11 OU placar_2 não for 11, repetir:
+	
+		// gera dado_1
+		// gera dado_2
+		
+		// se dado_1 > dado_2
+			// placar_1 incrementado
+		// se dado_1 < dado_2
+			// placa2_2 incrementado
+			
+		// ser for igual (nem preciso checar)
+			// ninguém ganha ponto
+	
+	// se placa_1 for 11
+		// escrever que o jogador 1 ganhou
+	// ou então
+		// escrever que o jogador 2 ganhou
+
     
     ale = rand() % 6+1 ; 
     
