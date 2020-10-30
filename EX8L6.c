c6a4c1de7b1db0b1660ceb1df42c7b4116ad9f65
Melina Almeida <melina.almeida@unesp.br>
2020-10-30 17:15:46 -0300

    Primeiro exercicio

---
+++ EX8L6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int dado_1, dado_2, placar_1, placar_2;
+    
+    ale = rand() % 6+1 ; 
+    
+    
+    system("PAUSE");
+    return 0;
+}
+
+
