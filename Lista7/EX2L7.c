a6be68192c59318cae5e8834dbb4925e26a3d9f7
Melina Almeida <melina.almeida@unesp.br>
2020-11-04 16:04:40 -0300

    Mudanças

---
+++ Lista7/EX2L7.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+  float nota;
+  
+  printf("Informe a nota do aluno ");
+  scanf("%f", &nota);
+  
+  if (nota<3){
+    printf("O conceito desse aluno eh E.\n");          
+  }
+  else if (nota>=3 || nota<=5){
+    printf("O conceito desse aluno eh D.\n");     
+  }
+  else if (nota>=6 || nota<=7){
+    printf("O conceito desse aluno eh C.\n");
+  }
+  else if (nota>=8 || nota<=9){
+    printf("O conceito desse aluno eh B.\n");      
+  }
+  else {
+    prinf("O conceito desse aluno eh A.\n");
+  
+ system("PAUSE"); 
+ return 0;   
+    
+}
