4b2c4521819261cbbf8fdab2eedf70ce29815d56
Melina Almeida <melina.almeida@unesp.br>
2020-11-06 18:35:38 -0300

    Lista 7

--- Lista 7/EX12.c
+++ Lista 7/EX12.c
@@ -3,53 +3,66 @@
 
 int main()
 {
- int codigo;
- float p1=1.50, p2=3.50, p3=3.00, p4=2.50, p5=3.75, p6=4.50, p7=5.50, p8=7.00, p9=8.00, p10=13.99;  
+ int codigo, n; 
+ float preco;
  
-
- printf("001---------Refri Pequeno--------------RS1.50\n");
- printf("002----------Refri Grande--------------RS3.50\n");
- printf("003--------Suco de Laranja-------------RS3.00\n");
- printf("004------------Casquinha---------------RS2.50\n");
- printf("005------Casquinha c/ Cobertura--------RS3.75\n");
- printf("006-------Casquinha Dupla--------------RS4.50\n");
- printf("007---Casquinha Dupla c/ Cobertura-----RS5.50\n");
- printf("008------------Sundae------------------RS7.00\n");
- printf("009---------Banana Split---------------RS8.00\n");
- printf("010--------Especial da Casa------------RS13.99\n");
+ printf("      SORVETERIA FLOCO DE NEVE               ");
+ printf("\n\n");
+ printf("1---------Refri Pequeno--------------RS1.50\n");
+ printf("2----------Refri Grande--------------RS3.50\n");
+ printf("3--------Suco de Laranja-------------RS3.00\n");
+ printf("4------------Casquinha---------------RS2.50\n");
+ printf("5------Casquinha c/ Cobertura--------RS3.75\n");
+ printf("6-------Casquinha Dupla--------------RS4.50\n");
+ printf("7---Casquinha Dupla c/ Cobertura-----RS5.50\n");
+ printf("8------------Sundae------------------RS7.00\n");
+ printf("9---------Banana Split---------------RS8.00\n");
+ printf("10-------Especial da Casa------------RS13.99\n");
   
- printf("Digite o codigo do produto desejado ");
+ printf("\n\nDigite o codigo do produto desejado ");
  scanf("%d", &codigo);
-
- if(codigo==001){
-   printf("O preco a pagar eh RS1.50\n");              
+ printf("Quantas unidades deseja?");
+ scanf("%d", &n);
+ 
+ if(codigo==1){
+   preco = 1.50*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);              
  }
-  if(codigo==002){
-   printf("O preco a pagar eh RS3.50\n");                 
+  if(codigo==2){
+   preco = 3.50*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                
  }
-  if(codigo==003){
-   printf("O preco a pagar eh RS3.00\n");                 
+  if(codigo==3){
+   preco = 3.00*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                 
  }
-  if(codigo==004){
-   printf("O preco a pagar eh RS2.50\n");                 
+  if(codigo==4){
+   preco = 2.50*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);               
  }
-  if(codigo==005){
-   printf("O preco a pagar eh RS3.75\n");                 
+  if(codigo==5){
+   preco = 3.75*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                  
  }
-  if(codigo==006){
-  printf("O preco a pagar eh RS4.50\n");                  
+  if(codigo==6){
+   preco = 4.50*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                 
  }
-  if(codigo==007){
-   printf("O preco a pagar eh RS5.50\n");                 
+  if(codigo==7){
+   preco = 5.50*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                  
  }
-  if(codigo==008){
-   printf("O preco a pagar eh RS7.00\n");                 
+  if(codigo==8){
+   preco = 7.00*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                
  }
-  if(codigo==009){
-   printf("O preco a pagar eh RS8.00\n");                 
+  if(codigo==9){
+   preco = 9.00*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                 
  }
-  if(codigo==010){
-   printf("O preco a pagar eh RS13.99\n");                 
+  if(codigo==10){
+   preco = 13.99*n ;            
+   printf("\nO preco a pagar eh RS %.2f. Volte sempre!\n\n", preco);                
  }
 
  system("PAUSE"); 
--- Lista 7/EX6.c
+++ Lista 7/EX6.c
@@ -1,11 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
 
 int main()
 {
+  int opcao, sair;
+  float raio, perimetro, area;
+          
+  printf("       Escolha uma opcao abaixo: ");
+  printf("\n\n1- Calculo da area da circunferencia; \n");
+  printf("2- Calculo do volume da circunferencia; \n");
+  printf("0- Nao to afim de calcular porcaria nenhuma.\n");
+  
+  scanf("%d", &opcao);
+  
+  switch(opcao) {
+    case 1:
+    printf("\nDigite o raio desejado ");
+    scanf("%f", &raio);
+    area = 3.1415*pow(raio,2);
+    printf("A area eh %.2f\n\n", area); 
+    break;
+			
+    case 2:
+    printf("\nDigite o raio desejado ");
+    scanf("%f", &raio);  
+    perimetro = 2*3.1415*raio;
+    printf("\nO perimetro eh %.2f\n\n", perimetro);
+    break;
+				
+    case 0:
+    printf("Vlw flw\n");
+    sair = 1;
+    break;
+				
+    default:
+    printf("Mano... Escolhe qlqr numero da lista ai...\n");	
+  }
  
-
-
  system("PAUSE"); 
  return 0;   
     
