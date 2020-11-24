a32d510068b1d67d9735b3b36086215d6ad7a49c
Marco Antonio Almeida Silva <silva_almeida@terceiros.sicredi.com.br>
2020-11-23 21:24:04 -0300

    calculo de incremento de data

--- Lista 8/EX7.c
+++ Lista 8/EX7.c
@@ -1,37 +1,161 @@
 #include<stdlib.h>
 #include<stdio.h> 
+#include <stdbool.h>
+
+typedef struct Data{
+	int ano;
+	int mes;
+	int dia;
+	
+	bool erro;
+	char *msg;
+};
+
+bool checa_bissexto(int ano){
+	return (ano%400==0 || (ano%4==0 && ano%100!=0));
+}
+
+struct Data incrementa_dia(struct Data ent){
+	struct Data saida;
+
+	saida.erro = false;
+	
+	saida.ano = ent.ano;
+	saida.mes = ent.mes;
+	saida.dia = ent.dia;
+	
+	if ((ent.ano<1)||(ent.ano>2100)){
+		saida.erro = true;
+		saida.msg ="ano deve ser entre 1 e 2100";
+		return saida;
+	}
+	
+	if ((ent.mes<1)||(ent.mes>12)){
+		saida.erro = true;
+		saida.msg ="mes deve ser entre 1 e 12";
+		return saida;
+	}
+
+	if (ent.mes==2){
+		if (checa_bissexto(ent.ano)){
+			if ((ent.dia<1)||(ent.dia>29)){
+				saida.erro = true;
+				saida.msg ="Os dias no mes de fev em ano bissexto devem ser entre 1 e 29";
+				return saida;
+			}
+			if (ent.dia==29){
+				saida.dia = 1;
+				saida.mes = 3;
+				saida.ano = saida.ano;
+				
+			}else{
+				saida.dia = saida.dia+1;
+				saida.mes = saida.mes;
+				saida.ano = saida.ano;
+				
+			}
+			
+		}else{
+			if ((ent.dia<1)||(ent.dia>28)){
+				saida.erro = true;
+				saida.msg ="Os dias no mes de fev en ano nao bissexto devem ser entre 1 e 28";
+				return saida;
+			}
+			if (ent.dia==28){
+				saida.dia = 1;
+				saida.mes = 3;
+				saida.ano = saida.ano;
+				
+			}else{
+				saida.dia = saida.dia+1;
+				saida.mes = saida.mes;
+				saida.ano = saida.ano;
+				
+			}
+			
+		}
+		
+		
+	}else	if ((ent.mes==1)||
+						(ent.mes==3)||
+						(ent.mes==5)||
+						(ent.mes==7)||
+						(ent.mes==8)||
+						(ent.mes==10)||
+						(ent.mes==12)){
+		if ((ent.dia<1)||(ent.dia>31)){
+			saida.erro = true;
+			saida.msg ="Os dias nos meses de jan, mar, mai, jul, ago, out, dez devem ser entre 1 e 31";
+			return saida;
+
+		}
+		if (ent.dia==31){
+			if (ent.mes==12){
+				saida.dia = 1;
+				saida.mes = 1;
+				saida.ano = saida.ano+1;
+				
+			}else{
+				saida.dia = 1;
+				saida.mes = saida.mes+1;
+				saida.ano = saida.ano;
+				
+			}
+
+		}else{
+			saida.dia = ent.dia+1;
+			saida.mes = ent.mes;
+			saida.ano = ent.ano;
+
+		}
+				
+	} else {
+		if ((ent.dia<1)||(ent.dia>30)){
+			saida.erro = true;
+			saida.msg ="Os dias nos meses de abr, jun, set, nov devem ser entre 1 e 30";
+			return saida;
+
+		}
+		if (ent.dia==30){
+			saida.dia = 1;
+			saida.mes = saida.mes+1;
+			saida.ano = saida.ano;
+			
+		}else{
+			saida.dia = ent.dia+1;
+			saida.mes = ent.mes;
+			saida.ano = ent.ano;
+
+		}
+	
+	}
+	
+	
+	return saida;
+}
+
 
 int main()
 {
- 
-/*
- função que tenha 3 entradas e 3 saidas
- dia/ mes/ ano 
- 
- a função modifica os numeros de forma que apareça 
- o dia seguinte 
- (dia atual mais 1)
- 
- 
- condições especificas
- 
- se mes=fevereiro => 28 dias
- 
- se mes=janeiro => 31 dias
- se mes=março => 31 dias
- se mes=maio => 31 dias
- se mes=julho => 31 dias
- se mes=agosto => 31 dias
- se mes=outubro => 31 dias
- se mes=outubro => 31 dias
- se mes=dezembro => 31 dias
- 
- se nao => 30 dias
- 
- se dia=28 ou 30 ou 31 => passar para o
-  prox mes
-*/
- 
+	struct Data entrada;
+	struct Data incrementada;
+	
+	printf("A data deve ser entrada no formato (dd-mm-yyyy) (e.g. 29-10-1973, 29-02-2000...) \n");
+	printf("Por favor, entre a data : ");
+	scanf("%d-%d-%d", &entrada.dia, &entrada.mes, &entrada.ano);
+	
+	incrementada = incrementa_dia(entrada);
+	
+	if (!incrementada.erro){
+		printf("O dia seguinte a %02d-%02d-%03d eh %02d-%02d-%04d \n \n",
+			entrada.dia, entrada.mes, entrada.ano,
+			incrementada.dia, incrementada.mes, incrementada.ano);
+			
+  }else{
+		printf("erro na data \n%s \n \n", incrementada.msg);
+  	
+	}
+	
   system("PAUSE");
   return 0;   
     
